389834dd2f259c144929a43c372d306e9027f5de
KarisZhao <347538584@qq.com>
2017-02-14 11:48:21 +0800

    第二次提交

--- SnapKitDemo/SnapKitDemo/BasicViewController.swift
+++ SnapKitDemo/SnapKitDemo/BasicViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-//import SnapKit
+import SnapKit
 
 class BasicViewController: UIViewController {
 
@@ -22,11 +22,26 @@ class BasicViewController: UIViewController {
 
     func setUI() {
         let baseView = UIView()
-        baseView.backgroundColor = UIColor.white
+        baseView.backgroundColor = UIColor.red
         self.view.addSubview(baseView)
+        //布局背景
+        baseView.snp.remakeConstraints { (make) in
+            make.edges.equalTo(self.view).inset(UIEdgeInsetsMake(84, 20, 20, 20))
+        }
         
-//        baseView.
-        
+        for index in 1...5 {
+            let aView = UIView()
+            let red = CGFloat(arc4random_uniform(255))/CGFloat(255.0)
+            let green = CGFloat( arc4random_uniform(255))/CGFloat(255.0)
+            let blue = CGFloat(arc4random_uniform(255))/CGFloat(255.0)
+            aView.backgroundColor = UIColor.init(red: red, green: green, blue: blue, alpha: 1)
+            self.view.addSubview(aView)
+            aView.snp.makeConstraints({ (make) in
+                make.center.equalTo(baseView)
+                make.left.equalTo(baseView).offset((20*index))
+                make.top.equalTo(baseView).offset((20*index))
+            })
+        }
     }
     
     override func didReceiveMemoryWarning() {
